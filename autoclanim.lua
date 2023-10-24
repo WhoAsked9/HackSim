906fee8674f994d1e65025c71a2cee289052cb4b
WhoAsked9 <134094752+WhoAsked9@users.noreply.github.com>
2023-10-24 21:36:20 +0500

    Create autoclanim.lua

---
+++ autoclanim.lua
@@ -0,0 +1,78 @@
+while true do
+  wait(0.1)
+  local args = {
+    [1] = "Gift1"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift2"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift3"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift4"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift5"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift6"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift7"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift8"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift9"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift10"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift11"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift12"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift13"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift14"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+  local args = {
+    [1] = "Gift15"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ClaimGift"):FireServer(unpack(args))
+end
